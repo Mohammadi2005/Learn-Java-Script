62844d96c52e98115e29bfc898fda7d341becf33
amir hossein <amohammadi161183@gmail.com>
2025-02-02 09:14:48 +0330

    array

---
+++ JS/arr.js
@@ -0,0 +1,18 @@
+// port 3 : arr
+document.write("form 1 : <br />");
+var Names = ["amir", "ahmad", "ali", "sara", "mahdi"];
+
+document.write(Names[0] + "<br />" + Names[1] + "<br /><br />");
+document.write("length Names Array is : " + Names.length + "<br /><br />");
+document.write(Names);
+
+
+document.write("<br /><br />form 2 : <br />");
+var Names2 = new Array("amir", "ahmad", "ali", "sara", "mahdi");
+
+document.write(Names2[0] + "<br />" + Names2[1] + "<br /><br />");
+document.write("length Names Array is : " + Names2.length + "<br /><br />");
+document.write(Names2);
+
+
+
