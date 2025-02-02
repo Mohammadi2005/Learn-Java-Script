a171972eb98f96cb7121e53a300376a16e1c4230
amir hossein <amohammadi161183@gmail.com>
2025-02-02 09:13:01 +0330

    variable

---
+++ JS/var.js
@@ -0,0 +1,12 @@
+// part 1: variable
+
+var Name = 'amir hossein';
+var age = 20;
+var is_student = true;
+
+
+alert(Name + " is " + typeof Name);
+alert(age + " is " + typeof age);
+alert(is_student + " is " + typeof is_student);
+
+
