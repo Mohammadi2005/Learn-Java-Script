1bfdae5f97e1319821382443a6353397a406c761
amir hossein <amohammadi161183@gmail.com>
2025-02-02 09:15:54 +0330

    if else

---
+++ JS/if.js
@@ -0,0 +1,14 @@
+var x = 21;
+
+if (x <= 10) {
+    alert("Good Morning");
+}
+else if (x <= 15) {
+    alert("Good Day");
+}
+else if (x <= 19) {
+    alert("Good Evneng");
+}
+else {
+    alert("Good Night");
+}
